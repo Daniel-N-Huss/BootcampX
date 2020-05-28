94a780d4b70295ca763f5c282ee8b3798f3038fb
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-28 04:41:49 +0000

    sql injection and app based sql query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,15 +8,17 @@ const pool = new Pool({
 });
 
 
-
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 
 pool.query(`
 SELECT students.id AS id, students.name AS name, cohorts.name AS cohort
 FROM students
 JOIN cohorts on cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${process.argv[2]}%'
-LIMIT ${process.argv[3]};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
